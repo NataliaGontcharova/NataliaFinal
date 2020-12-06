dd3b6d9c88077a9075873488c658fafd7a715413
NataliaGontcharova <gont0004@algonquinlive.com>
2020-12-06 13:37:02 -0500

    Comments

--- likes.js
+++ likes.js
@@ -1,13 +1,34 @@
+// Functions that make possible setting the Like flag for a movie
+// The flag setting is stored as key-value pair in a local storage in the format
+//  <movie-title> => "yes"       : for liked movie
+//  <movie-title> => "no" or ""  : for movies that don't have the like flag
+
+
+// The scripts detects the movie title of the Page title element
+// expected to contain the name
 var movieOnThePage = document.getElementById("movieTitle").innerText;
 
+
+
+// Read the like flag off local storage
+// The expected returned values: 
+//  "yes"       => has a Like flag
+//  "no" or ""  => doesn't have the Like flag
 function ReadLike(movie) {
     return localStorage.getItem(movie);
 }
+// Save the like flag state for a given movie
+// "movie" parameter is to pass the title of the movie
+// "liked" parameter is to pass the flag state
 function SaveLike(movie, liked) {
     localStorage.setItem(movie, liked);
 }
 
 
+// Reflect the like/non-liked setting on the page
+// Save the current state in local file storage
+// "movie" parameter is to pass the title of the movie
+// "liked" parameter is a boolean. "true" for liked pages
 function SetLiked(movie, liked) {
     var img = document.getElementById("heartToggleImg");
     if (liked) {
@@ -19,7 +40,18 @@ function SetLiked(movie, liked) {
         SaveLike(movie, "no");
     }
 }
+
+
 function ToggleLiked(movie) {
+    // Read current state of the Liked flag off local stoage
+    // Flip its value to the opposite
+    // Note, that "" and "no" both represent non-Liked state of the movie
     SetLiked(movie, !(ReadLike(movie) === "yes"));
 }
+
+// The call executing during page loading, 
+// which assumes the script is referenced after the like/heartToggling image declaration on the page
+// so by the time this call is executed, needed image element is available
+// The logic here checks if the local storage contains like save for a movie for this page
+// and re-applies appropriate Like image this session to restore the state of Like/Heart flag
 SetLiked(movieOnThePage, ReadLike(movieOnThePage) === "yes");
--- theme.js
+++ theme.js
@@ -1,14 +1,34 @@
+// Functions for setting, toggling Day/Light themes
+// and saving the theme choice as cookie
+
+// Read theme name off a cookie value
+// Cookie format is expected to be:  
+//   theme=dark  or theme=light
+// So we extract the theme name starting with position 6
+//   to get "dark" or "light" in the function result
 function ReadTheme() {
     return document.cookie.substring(6);
 }
+
+// Saving theme value on a cookie in the format:
+//  theme=dark or theme=light
 function SaveTheme(themeType) {
     document.cookie = "theme=" + themeType;
 }
 
-
+// Apply the theme setting to the document
+//   - apply the background color to the <body> element
+//   - set corresponding image for the menu Icon image for them toggling       
 function SetDayNightTheme(isDark) {
+    // find the body element by tag name (document could have only one body)
     var body = document.getElementsByTagName("BODY")[0];
+    // find the icon image by ID we assign on the page(s)
     var img = document.getElementById("themeToggleImg");
+
+    // Depending the on the them type, apply corresponding color/image
+    // The calling SaveTheme() to save the choice made, which is needed for 
+    //  - subsequent toggling  (we read current setting of a cookie)
+    //  - remembering the choice the next time the page is opened
     if (isDark) {
         body.style.background = "black";
         img.src = "Images/bulb-outline.svg";
@@ -21,6 +41,14 @@ function SetDayNightTheme(isDark) {
     }
 }
 function ToggleDayNight() {
+    // Read current theme off a cookie
+    // Switch to the another option
     SetDayNightTheme(!(ReadTheme() === "dark"));
 }
+
+// The call executing during page loading, 
+// which assumes the script is referenced after body and menu ICON image declaration on the page
+// so by the time this call is executed, needed html elements are available
+// The logic here reads off a cookie the theme choice made when this web site was used last time
+// and re-applies the theme for this session
 SetDayNightTheme(ReadTheme() === "dark");
